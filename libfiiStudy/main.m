e1c0e16d26658836772f48269672f49c7b2113a8
孟冰川 <mengbingchuan@36kr.com>
2019-05-07 16:26:07 +0800

    Initial Commit

---
+++ libfiiStudy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  libfiiStudy
+//
+//  Created by 孟冰川 on 2019/5/7.
+//  Copyright © 2019 孟冰川. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
